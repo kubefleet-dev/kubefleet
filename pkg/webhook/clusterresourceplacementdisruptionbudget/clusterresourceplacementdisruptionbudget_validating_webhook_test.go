56d4c01a9edf1807c25af0d5a6630187fc3b5bef
Britania Rodriguez Reyes <britaniar@microsoft.com>
2025-05-19 19:06:12 -0700

    minor fixes
    Signed-off-by: Britania Rodriguez Reyes <britaniar@microsoft.com>

--- pkg/webhook/clusterresourceplacementdisruptionbudget/clusterresourceplacementdisruptionbudget_validating_webhook_test.go
+++ pkg/webhook/clusterresourceplacementdisruptionbudget/clusterresourceplacementdisruptionbudget_validating_webhook_test.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2025 The KubeFleet Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package clusterresourceplacementdisruptionbudget
 
 import (
@@ -6,6 +19,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	admissionv1 "k8s.io/api/admission/v1"
 	authenticationv1 "k8s.io/api/authentication/v1"
@@ -293,7 +307,9 @@ func TestHandle(t *testing.T) {
 	for testName, testCase := range testCases {
 		t.Run(testName, func(t *testing.T) {
 			gotResult := testCase.resourceValidator.Handle(context.Background(), testCase.req)
-			assert.Equal(t, testCase.wantResponse, gotResult, utils.TestCaseMsg, testName)
+			if diff := cmp.Diff(testCase.wantResponse, gotResult); diff != "" {
+				t.Errorf("ClusterResourcePlacementDisruptionBudgetValidator Handle() mismatch (-want +got):\n%s", diff)
+			}
 		})
 	}
 }
