698f008c22ef380d14f610de5e1dfdeba38f1fc0
Zhiying Lin <zhiyingl456@gmail.com>
2025-06-27 19:53:34 +0800

    update the api comment
    Signed-off-by: Zhiying Lin <zhiyingl456@gmail.com>

--- apis/placement/v1/resourcesnapshot_types.go
+++ apis/placement/v1/resourcesnapshot_types.go
@@ -53,7 +53,7 @@ import (
 //   - `SubindexOfResourceSnapshotAnnotation` to store the subindex of resource snapshot in the group.
 //
 // Snapshot may have the following annotations to indicate the time of next resourceSnapshot candidate detected by the controller:
-//   - `NextResourceSnapshotCandidateDetectionTime` to store the time of next resourceSnapshot candidate detected by the controller.
+//   - `NextResourceSnapshotCandidateDetectionTimeAnnotation` to store the time of next resourceSnapshot candidate detected by the controller.
 type ClusterResourceSnapshot struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
