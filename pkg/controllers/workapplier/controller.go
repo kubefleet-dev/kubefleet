1145a44d8df7643b067b888cf98ce4b588d7162f
Nicholas Laureano <nicholasray345@gmail.com>
2025-07-09 13:55:18 -0700

    controller.go unnecessary change reverted
    Signed-off-by: Nicholas Laureano <nicholasray345@gmail.com>

--- pkg/controllers/workapplier/controller.go
+++ pkg/controllers/workapplier/controller.go
@@ -312,7 +312,7 @@ const (
 	// Note that the reason string below uses the same value as kept in the old work applier.
 	ManifestProcessingAvailabilityResultTypeNotYetAvailable ManifestProcessingAvailabilityResultType = "ManifestNotAvailableYet"
 
-	ManifestProcessingAvailabilityResultTypeNotTrackable ManifestProcessingAvailabilityResultType = WorkNotAllManifestsTrackableReason 
+	ManifestProcessingAvailabilityResultTypeNotTrackable ManifestProcessingAvailabilityResultType = "NotTrackable"
 
 	// The descriptions for completed availability checks.
 	ManifestProcessingAvailabilityResultTypeAvailableDescription       = "Manifest is available"
