19f4086e65fe34bc346b58893bfe9b028cf0bfe5
Arvind Thirumurugan <arvindth@microsoft.com>
2025-08-27 14:24:47 -0700

    watcher for CRPS delete events
    Signed-off-by: Arvind Thirumurugan <arvindth@microsoft.com>

---
+++ pkg/controllers/clusterresourceplacementstatuswatcher/watcher.go
@@ -0,0 +1,127 @@
+/*
+Copyright 2024 The Kubernetes Fleet Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package clusterresourceplacementstatuswatcher
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	placementv1beta1 "github.com/kubefleet-dev/kubefleet/apis/placement/v1beta1"
+	"github.com/kubefleet-dev/kubefleet/pkg/utils/controller"
+)
+
+// Reconciler reconciles ClusterResourcePlacementStatus delete events.
+type Reconciler struct {
+	client.Client
+
+	// PlacementController maintains a rate limited queue which used to store
+	// the name of the placement objects and a reconcile function to consume the items in queue.
+	PlacementController controller.Controller
+}
+
+// Reconcile handles ClusterResourcePlacementStatus delete events by checking if the
+// corresponding ClusterResourcePlacement still exists and triggering reconciliation if needed.
+func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	startTime := time.Now()
+	crpsRef := klog.KObj(&placementv1beta1.ClusterResourcePlacementStatus{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      req.Name,
+			Namespace: req.Namespace,
+		},
+	})
+	klog.V(2).InfoS("ClusterResourcePlacementStatus watcher reconciliation starts", "crps", crpsRef)
+	defer func() {
+		latency := time.Since(startTime).Milliseconds()
+		klog.V(2).InfoS("ClusterResourcePlacementStatus watcher reconciliation ends", "crps", crpsRef, "latency", latency)
+	}()
+
+	// The CRPS was deleted, so we need to check if the corresponding CRP still exists
+	// and doesn't have a deletion timestamp. If so, we should trigger CRP reconciliation
+	// to recreate the CRPS.
+	crpName := req.Name // CRPS has the same name as its parent CRP
+	crp := &placementv1beta1.ClusterResourcePlacement{}
+
+	if err := r.Get(ctx, types.NamespacedName{Name: crpName}, crp); err != nil {
+		if apierrors.IsNotFound(err) {
+			// CRP doesn't exist, this is expected cleanup - no action needed
+			klog.V(2).InfoS("CRP not found, expected cleanup", "crp", crpName, "crps", crpsRef)
+			return ctrl.Result{}, nil
+		}
+		klog.ErrorS(err, "Failed to get CRP", "crp", crpName, "crps", crpsRef)
+		return ctrl.Result{}, err
+	}
+
+	// Check if CRP is being deleted
+	if crp.GetDeletionTimestamp() != nil {
+		// CRP is being deleted, CRPS deletion is expected - no action needed
+		klog.V(2).InfoS("CRP is being deleted, CRPS deletion is expected", "crp", klog.KObj(crp), "crps", crpsRef)
+		return ctrl.Result{}, nil
+	}
+
+	// CRP exists and is not being deleted, but its CRPS was deleted
+	// This might be an accidental deletion or external cleanup
+	// Trigger CRP reconciliation to recreate the CRPS
+	klog.V(2).InfoS("CRP exists without deletion timestamp, enqueueing for reconciliation to recreate CRPS", "crp", klog.KObj(crp), "crps", crpsRef)
+	r.PlacementController.Enqueue(controller.GetObjectKeyFromNamespaceName(crp.GetNamespace(), crp.GetName()))
+
+	return ctrl.Result{}, nil
+}
+
+// buildDeleteEventPredicate creates a predicate that only triggers on actual delete events
+// (when objects are removed from storage).
+func buildDeleteEventPredicate() predicate.Predicate {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			// Ignore creation events
+			return false
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			// Ignore update events
+			return false
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			// Only trigger on actual deletion events
+			if e.Object == nil {
+				klog.ErrorS(fmt.Errorf("delete event object is nil"), "Failed to process delete event")
+				return false
+			}
+
+			crpsRef := klog.KObj(e.Object)
+			klog.V(3).InfoS("CRPS delete event received", "crps", crpsRef)
+			return true
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			// Ignore generic events
+			return false
+		},
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).Named("clusterresourceplacementstatus-watcher").
+		For(&placementv1beta1.ClusterResourcePlacementStatus{}).
+		WithEventFilter(buildDeleteEventPredicate()).
+		Complete(r)
+}
---
+++ pkg/controllers/clusterresourceplacementstatuswatcher/watcher_integration_test.go
@@ -0,0 +1,200 @@
+/*
+Copyright 2025 The KubeFleet Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package clusterresourceplacementstatuswatcher
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+
+	fleetv1beta1 "github.com/kubefleet-dev/kubefleet/apis/placement/v1beta1"
+)
+
+const (
+	eventuallyTimeout         = time.Second * 20
+	consistentlyDuration      = time.Second * 5
+	consistentlyCheckInterval = time.Millisecond * 250
+	eventuallyCheckInterval   = time.Millisecond * 250
+)
+
+// This container cannot be run in parallel with other ITs because it uses a shared fakePlacementController.
+var _ = Describe("Test ClusterResourcePlacementStatus Watcher - delete events", Serial, func() {
+	var crp *fleetv1beta1.ClusterResourcePlacement
+	var crps *fleetv1beta1.ClusterResourcePlacementStatus
+
+	BeforeEach(func() {
+		fakePlacementController.ResetQueue()
+	})
+
+	Context("When CRPS is deleted but CRP still exists", func() {
+		It("Should enqueue CRP for reconciliation", func() {
+			testCRPName := "test-crp-1"
+
+			By("Creating a ClusterResourcePlacement")
+			crp = &fleetv1beta1.ClusterResourcePlacement{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: testCRPName,
+				},
+				Spec: fleetv1beta1.PlacementSpec{
+					StatusReportingScope: fleetv1beta1.NamespaceAccessible,
+					ResourceSelectors: []fleetv1beta1.ResourceSelectorTerm{
+						{
+							Group:   "",
+							Version: "v1",
+							Kind:    "Namespace",
+							Name:    testNamespace,
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, crp)).Should(Succeed(), "failed to create ClusterResourcePlacement")
+
+			By("Creating a ClusterResourcePlacementStatus in the target namespace")
+			crps = &fleetv1beta1.ClusterResourcePlacementStatus{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testCRPName, // Same name as CRP
+					Namespace: testNamespace,
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion:         fleetv1beta1.GroupVersion.String(),
+							Kind:               "ClusterResourcePlacement",
+							Name:               testCRPName,
+							UID:                crp.UID,
+							Controller:         ptr.To(true),
+							BlockOwnerDeletion: ptr.To(true),
+						},
+					},
+				},
+				PlacementStatus: fleetv1beta1.PlacementStatus{
+					ObservedResourceIndex: "0",
+				},
+				LastUpdatedTime: metav1.Now(),
+			}
+			Expect(k8sClient.Create(ctx, crps)).Should(Succeed(), "failed to create ClusterResourcePlacementStatus")
+
+			By("Ensuring placement controller queue is initially empty")
+			consistentlyCheckPlacementControllerQueueIsEmpty()
+
+			By("Deleting the ClusterResourcePlacementStatus (simulating accidental deletion)")
+			Expect(k8sClient.Delete(ctx, crps)).Should(Succeed(), "failed to delete ClusterResourcePlacementStatus")
+
+			By("Checking that CRP is enqueued for reconciliation")
+			Eventually(func() string {
+				return fakePlacementController.Key()
+			}, eventuallyTimeout, eventuallyCheckInterval).Should(Equal(testCRPName), "CRP should be enqueued for reconciliation")
+
+			By("Cleaning up the ClusterResourcePlacement")
+			Expect(k8sClient.Delete(ctx, crp)).Should(Succeed(), "failed to delete ClusterResourcePlacement")
+		})
+	})
+
+	Context("When CRPS is deleted and CRP is also being deleted", func() {
+		It("Should NOT enqueue CRP for reconciliation", func() {
+			testCRPName := "test-crp-2"
+
+			By("Creating a ClusterResourcePlacement")
+			crp = &fleetv1beta1.ClusterResourcePlacement{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:       testCRPName,
+					Finalizers: []string{"test-finalizer"}, // Add finalizer to prevent immediate deletion
+				},
+				Spec: fleetv1beta1.PlacementSpec{
+					StatusReportingScope: fleetv1beta1.NamespaceAccessible,
+					ResourceSelectors: []fleetv1beta1.ResourceSelectorTerm{
+						{
+							Group:   "",
+							Version: "v1",
+							Kind:    "Namespace",
+							Name:    testNamespace,
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, crp)).Should(Succeed(), "failed to create ClusterResourcePlacement")
+
+			By("Creating a ClusterResourcePlacementStatus in the target namespace")
+			crps = &fleetv1beta1.ClusterResourcePlacementStatus{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      testCRPName, // Same name as CRP
+					Namespace: testNamespace,
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion:         fleetv1beta1.GroupVersion.String(),
+							Kind:               "ClusterResourcePlacement",
+							Name:               testCRPName,
+							UID:                crp.UID,
+							Controller:         ptr.To(true),
+							BlockOwnerDeletion: ptr.To(true),
+						},
+					},
+				},
+				PlacementStatus: fleetv1beta1.PlacementStatus{
+					ObservedResourceIndex: "0",
+				},
+				LastUpdatedTime: metav1.Now(),
+			}
+			Expect(k8sClient.Create(ctx, crps)).Should(Succeed(), "failed to create ClusterResourcePlacementStatus")
+
+			By("Deleting the ClusterResourcePlacement (will have deletionTimestamp due to finalizer)")
+			Expect(k8sClient.Delete(ctx, crp)).Should(Succeed(), "failed to delete ClusterResourcePlacement")
+
+			// Verify CRP has deletionTimestamp
+			updatedCRP := &fleetv1beta1.ClusterResourcePlacement{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: testCRPName}, updatedCRP)).Should(Succeed())
+			Expect(updatedCRP.DeletionTimestamp).ToNot(BeNil(), "CRP should have deletionTimestamp")
+
+			By("Deleting the ClusterResourcePlacementStatus")
+			Expect(k8sClient.Delete(ctx, crps)).Should(Succeed(), "failed to delete ClusterResourcePlacementStatus")
+
+			By("Checking that CRP is NOT enqueued for reconciliation")
+			consistentlyCheckPlacementControllerQueueIsEmpty()
+
+			By("Cleaning up by removing finalizer")
+			updatedCRP.Finalizers = []string{}
+			Expect(k8sClient.Update(ctx, updatedCRP)).Should(Succeed(), "failed to remove finalizer")
+		})
+	})
+
+	Context("When CRPS is deleted but CRP doesn't exist", func() {
+		It("Should NOT enqueue anything for reconciliation", func() {
+			By("Creating a ClusterResourcePlacementStatus without corresponding CRP")
+			crps = &fleetv1beta1.ClusterResourcePlacementStatus{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "non-existent-crp",
+					Namespace: testNamespace,
+				},
+				PlacementStatus: fleetv1beta1.PlacementStatus{
+					ObservedResourceIndex: "0",
+				},
+				LastUpdatedTime: metav1.Now(),
+			}
+			Expect(k8sClient.Create(ctx, crps)).Should(Succeed(), "failed to create ClusterResourcePlacementStatus")
+
+			By("Deleting the ClusterResourcePlacementStatus")
+			Expect(k8sClient.Delete(ctx, crps)).Should(Succeed(), "failed to delete ClusterResourcePlacementStatus")
+
+			By("Checking that nothing is enqueued for reconciliation")
+			consistentlyCheckPlacementControllerQueueIsEmpty()
+		})
+	})
+})
+
+func consistentlyCheckPlacementControllerQueueIsEmpty() {
+	Consistently(func() string {
+		return fakePlacementController.Key()
+	}, consistentlyDuration, consistentlyCheckInterval).Should(BeEmpty(), "placement controller queue should be empty")
+}
