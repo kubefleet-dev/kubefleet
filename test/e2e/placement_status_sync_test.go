9a767f0c849f1c64d1e84bdd4053c03b27641dae
Arvind Thirumurugan <arvindth@microsoft.com>
2025-08-27 11:52:42 -0700

    lint fix
    Signed-off-by: Arvind Thirumurugan <arvindth@microsoft.com>

--- test/e2e/placement_status_sync_test.go
+++ test/e2e/placement_status_sync_test.go
@@ -21,10 +21,10 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
