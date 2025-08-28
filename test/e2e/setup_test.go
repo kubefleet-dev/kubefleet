b27bdf4bd8e3cdbc0abdd9f82a9d41468fe8701a
Britania Rodriguez Reyes <britaniar@microsoft.com>
2025-08-28 17:23:58 -0500

    restore
    Signed-off-by: Britania Rodriguez Reyes <britaniar@microsoft.com>

--- test/e2e/setup_test.go
+++ test/e2e/setup_test.go
@@ -389,26 +389,26 @@ func maxDuration(a, b time.Duration) time.Duration {
 }
 
 func beforeSuiteForProcess1() {
-	//beforeSuiteForAllProcesses()
+	beforeSuiteForAllProcesses()
 
-	//setAllMemberClustersToJoin()
+	setAllMemberClustersToJoin()
 	checkIfAllMemberClustersHaveJoined()
-	//checkIfAzurePropertyProviderIsWorking()
+	checkIfAzurePropertyProviderIsWorking()
 
 	// Simulate that member cluster 4 become unhealthy, and member cluster 5 has left the fleet.
 	//
 	// Note that these clusters are not real kind clusters.
-	//setupInvalidClusters()
-	//createResourcesForFleetGuardRail()
-	//createTestResourceCRD()
+	setupInvalidClusters()
+	createResourcesForFleetGuardRail()
+	createTestResourceCRD()
 }
 
 var _ = SynchronizedBeforeSuite(beforeSuiteForProcess1, beforeSuiteForAllProcesses)
 
 var _ = SynchronizedAfterSuite(func() {}, func() {
-	//deleteResourcesForFleetGuardRail()
-	//deleteTestResourceCRD()
-	//setAllMemberClustersToLeave()
-	//checkIfAllMemberClustersHaveLeft()
-	//cleanupInvalidClusters()
+	deleteResourcesForFleetGuardRail()
+	deleteTestResourceCRD()
+	setAllMemberClustersToLeave()
+	checkIfAllMemberClustersHaveLeft()
+	cleanupInvalidClusters()
 })
