7f93790d4d9e880ffaa007d835e2114f770024b9
Arvind Thirumurugan <arvindth@microsoft.com>
2025-10-30 15:33:08 -0700

    minor fix
    Signed-off-by: Arvind Thirumurugan <arvindth@microsoft.com>

--- apis/placement/v1beta1/stageupdate_types.go
+++ apis/placement/v1beta1/stageupdate_types.go
@@ -185,9 +185,8 @@ type UpdateRunSpec struct {
 	// State indicates the desired state of the update run.
 	// When "Stop", the update run will initialize but not execute.
 	// When "Start", the update run will begin or continue execution.
-	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=Start;Stop
-	// +kubebuilder:default="Stop"
+	// +kubebuilder:default=Stop
 	State State `json:"state"`
 }
 
