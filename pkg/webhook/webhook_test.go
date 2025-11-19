c83ca1a1c1deb26934df810f73c85e70b5a14357
Wei Weng <Wei.Weng@microsoft.com>
2025-11-19 01:43:13 +0000

    fix unit test
    Signed-off-by: Wei Weng <Wei.Weng@microsoft.com>

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -52,7 +52,7 @@ func TestBuildFleetValidatingWebhooks(t *testing.T) {
 				serviceURL:           "test-url",
 				clientConnectionType: &url,
 			},
-			wantLength: 8,
+			wantLength: 6,
 		},
 	}
 
