0ff163fc75a06f6f2e82c77ca367c63f673fb726
Yetkin Timocin <ytimocin@microsoft.com>
2026-08-12 14:30:15 -0700

    test: gofmt import ordering in spike fake provisioner
    Signed-off-by: Yetkin Timocin <ytimocin@microsoft.com>

--- test/spike/clusterclaim/fakeprovisioner.go
+++ test/spike/clusterclaim/fakeprovisioner.go
@@ -22,10 +22,10 @@ import (
 	"maps"
 	"sync"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
-	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
